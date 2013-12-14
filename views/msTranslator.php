2cbe896293d013f413f486b672552aaaf124f5f8
dpecos <dpecos@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-14 09:24:18 +0000

    1.6: Fix in error handling code
    git-svn-id: http://plugins.svn.wordpress.org/wp-translation-box/trunk@821749 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/msTranslator.php
+++ views/msTranslator.php
@@ -52,7 +52,7 @@ class AccessTokenAuthentication {
             curl_close($ch);
             //Decode the returned JSON string.
             $objResponse = json_decode($strResponse);
-            if ($objResponse->error){
+            if (property_exists($objResponse, 'error')){
                 throw new Exception($objResponse->error_description);
             }
             return $objResponse->access_token;
